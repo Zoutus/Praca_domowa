7040977593babcef5f2618aa6521b7168d5ce4c6
Zoutus <125728891+Zoutus@users.noreply.github.com>
2023-06-15 18:43:38 +1000

    Add files via upload

---
+++ bloodsugar/measurements/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Measurement
+
+
+def measurement_list(request):
+    measurements = Measurement.objects.all()
+    return render(request, 'measurements/measurements_list.html', {'measurements': measurements})
+
+
+def measurement_details(request, id):
+    measurement = Measurement.objects.get(id=id)
+    return render(request, "measurements/measurements_details.html", {"measurement": measurement})
+
+
+def measurement_delete(request, id):
+    measurement = get_object_or_404(Measurement, pk=id)
+    if request.method == 'POST':
+        measurement_id = request.POST.get('measurement_id')
+        if request.POST.get('action') == 'delete':
+            Measurement.objects.get(id=id).delete()
+            return measurement_list(request)
+        elif request.POST.get('action') == 'back':
+            return redirect("measurement_list")
+    return render(request, 'measurements/measurements_delete.html', {"measurement": measurement})
